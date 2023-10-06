d8e81d755e619658d120967dc2000d7e2d4d5d11
Tyler Hunt <tyhunt82@users.noreply.github.com>
2023-10-06 20:41:54 +0000

    rt

--- app.py
+++ app.py
@@ -9,29 +9,32 @@ import streamlit_authenticator as stauth
 def main():
     st.title("Welcome to the SkulptGPT AI App")
 
+    # Read in yaml
     with open('config.yaml') as file:
         config = yaml.load(file, Loader=SafeLoader)
     
-   
+    authenticator = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days'],
+        config['preauthorized']
+    )
 
-    with st.sidebar:
-        authenticator = stauth.Authenticate(
-            config['credentials'],
-            config['cookie']['name'],
-            config['cookie']['key'],
-            config['cookie']['expiry_days'],
-            config['preauthorized']
-        )
+    authenticator.login('Login', 'main')
 
-        authenticator.login('Login', 'sidebar')
-
-        if st.session_state["authentication_status"]:
-            authenticator.logout('Logout', 'sidebar', key='unique_key')
+    if st.session_state["authentication_status"]:
+        with st.sidebar:
+            authenticator.logout('Logout', 'main', key='unique_key')
             st.write(f'Welcome *{st.session_state["name"]}*')
-        elif st.session_state["authentication_status"] is False:
-            st.error('Username/password is incorrect')
-        elif st.session_state["authentication_status"] is None:
-            st.warning('Please enter your username and password')
+
+    elif st.session_state["authentication_status"] is False:
+        st.error('Username/password is incorrect')
+        
+    elif st.session_state["authentication_status"] is None:
+        st.warning('Please enter your username and password')
+
+    
 
 if __name__ == "__main__":
     main()
