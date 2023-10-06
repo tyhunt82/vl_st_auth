0b29365762d9922461b8aa480e50ffba4208dbc0
Tyler Hunt <tyhunt82@users.noreply.github.com>
2023-10-06 20:56:01 +0000

    ma

--- app.py
+++ app.py
@@ -13,32 +13,32 @@ def main():
     with open('config.yaml') as file:
         config = yaml.load(file, Loader=SafeLoader)
     
-    authenticator = stauth.Authenticate(
-        config['credentials'],
-        config['cookie']['name'],
-        config['cookie']['key'],
-        config['cookie']['expiry_days'],
-        config['preauthorized']
-    )
-
-    authenticator.login('Login', 'main')
-    links = """
-        <div style="text-align: center;">
-            <a href="#" target="_blank"> Forgot Username</a>  | 
-            <a href="#" target="_blank"> Forgot Password</a>  | 
-            <a href="#" target="_blank"> Register New</a>
-        </div>
-        """
-    st.markdown(links, unsafe_allow_html=True)
+    
 
 
     if st.session_state["authentication_status"]:
         with st.sidebar:
             authenticator.logout('Logout', 'main', key='unique_key')
-            st.write(f'Welcome *{st.session_state["name"]}*')
+            # st.write(f'Welcome *{st.session_state["name"]}*')
 
     elif st.session_state["authentication_status"] is False:
-        st.error('Username/password is incorrect')
+        authenticator = stauth.Authenticate(
+            config['credentials'],
+            config['cookie']['name'],
+            config['cookie']['key'],
+            config['cookie']['expiry_days'],
+            config['preauthorized']
+        )
+
+        authenticator.login('Login', 'main')
+        links = """
+            <div style="text-align: center;">
+                <a href="#" target="_blank"> Forgot Username</a>  | 
+                <a href="#" target="_blank"> Forgot Password</a>  | 
+                <a href="#" target="_blank"> Register New</a>
+            </div>
+            """
+        st.markdown(links, unsafe_allow_html=True)
 
     elif st.session_state["authentication_status"] is None:
         st.warning('Please enter your username and password')
