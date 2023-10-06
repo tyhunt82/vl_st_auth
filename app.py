6d02ee622e068cbc75ef08d8fbcb4fa747e1d550
Tyler Hunt <tyhunt82@users.noreply.github.com>
2023-10-06 19:59:17 +0000

    update

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
+
+def main():
+    st.title("My First Streamlit App")
+    st.write("Hello, World!")
+
+
+    with open('config.yaml') as file:
+        config = yaml.load(file, Loader=SafeLoader)
+    
+    hashed_passwords = stauth.Hasher(['abc123', 'abc123']).generate()
+
+    authenticator = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days'],
+        config['preauthorized']
+    )
+
+    authenticator.login('Login', 'main')
+
+    with open('../config.yaml', 'w') as file:
+        yaml.dump(config, file, default_flow_style=False)
+
+    if st.session_state["authentication_status"]:
+        authenticator.logout('Logout', 'main', key='unique_key')
+        st.write(f'Welcome *{st.session_state["name"]}*')
+        st.title('Some content')
+    elif st.session_state["authentication_status"] is False:
+        st.error('Username/password is incorrect')
+    elif st.session_state["authentication_status"] is None:
+        st.warning('Please enter your username and password')
+
+if __name__ == "__main__":
+    main()
