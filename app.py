91afaffb633ff87f81efc519f5ffdddfd6a9b005
Tyler Hunt <tyhunt82@users.noreply.github.com>
2023-10-07 16:20:13 +0000

    update login

--- app.py
+++ app.py
@@ -2,36 +2,31 @@ import streamlit as st
 import yaml
 from yaml.loader import SafeLoader
 import streamlit_authenticator as stauth
-from components.login import get_login
+from components.login import get_login, get_config_read, get_config_write
 
 # References -----------------------------------------
 # Streamlit-autherticator -> https://github.com/mkhorasani/Streamlit-Authenticator?ref=blog.streamlit.io
 
-# set default case
+# Session vars
 if "authentication_status" not in st.session_state:
     st.session_state["authentication_status"] = False
+
+if "auth_config_read" not in st.session_state:
+    st.session_state["auth_config_read"] = get_config_read()
+
+if "auth_login" not in st.session_state:
+    st.session_state["auth_login"] = get_login(st.session_state["auth_config_read"])
 
 
 def main():
     st.title("Welcome to the SkulptGPT AI App")
 
     if st.session_state["authentication_status"]==False:
-        get_login()
+        st.session_state["auth_login"]
 
     elif st.session_state["authentication_status"]:
-        with st.sidebar:
-            if st.session_state["authentication_status"]:
-                st.write("Logged in")
-            with open('config.yaml') as file:
-                config = yaml.load(file, Loader=SafeLoader)
-            authenticator = stauth.Authenticate(
-                config['credentials'],
-                config['cookie']['name'],
-                config['cookie']['key'],
-                config['cookie']['expiry_days'],
-                config['preauthorized']
-            )
-            authenticator.logout('Logout', 'main', key='unique_key')
+        st.markdown("## SkulptGPT App")
+       
 
 
 if __name__ == "__main__":
--- components/login.py
+++ components/login.py
@@ -3,10 +3,17 @@ import yaml
 from yaml.loader import SafeLoader
 import streamlit_authenticator as stauth
 
-def get_login():
+def get_config_read():
     with open('config.yaml') as file:
         config = yaml.load(file, Loader=SafeLoader)
+    return config
 
+def get_config_write(config):
+    with open('config.yaml', 'w') as file:
+        yaml.dump(config, file, default_flow_style=False)
+    return config
+
+def get_login(config):
     login_cont = st.container()
 
     with login_cont:
@@ -27,3 +34,27 @@ def get_login():
         </div>
         """
         st.markdown(links, unsafe_allow_html=True)
+    return login_cont
+
+def get_logout(config):
+    logout_cont = st.container()
+
+    with logout_cont:
+        authenticator = stauth.Authenticate(
+                config['credentials'],
+                config['cookie']['name'],
+                config['cookie']['key'],
+                config['cookie']['expiry_days'],
+                config['preauthorized']
+            )
+
+        authenticator.logout('Logout', 'sidebar', key='unique_key')
+        links = """
+        <div style="text-align: center;">
+            <a href="#" target="_blank"> Forgot Username</a>  | 
+            <a href="#" target="_blank"> Forgot Password</a>  | 
+            <a href="#" target="_blank"> Register New</a>
+        </div>
+        """
+        st.markdown(links, unsafe_allow_html=True)
+    return logout_cont
